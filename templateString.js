550ba19fa8b495e45e5275675f0a0e89df06246c
Al-Amin49 <alamin496832@gmail.com>
2021-02-11 10:46:22 +0600

    learned Arrow function

---
+++ templateString.js
@@ -0,0 +1,15 @@
+const firstName ="Ali";
+const lastName ="baba";
+const fullName = firstName+ " " + lastName + " is a big company in the world";
+const fullName2=`${firstName} ${lastName} is good company`;
+const fullname3 =`${firstName} ${34+56+10-23}@gmail.com`;
+console.log(fullName);
+console.log(fullName2);
+console.log(fullname3);
+
+//Multiline
+const multiline = "I love you" + " I miss you" + " I need you";
+console.log(multiline); // ata new line a korte chaile amra \n dei kintu es6 a arekta systema ase ja niche dewa holo.
+
+const multiline2= `I love you I miss you no. But I don't need you now. `
+console.log(multiline2);
